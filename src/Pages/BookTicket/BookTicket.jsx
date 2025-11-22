1438a755898fabf2413720f5864dce78ca5fa5cd
MuhammadKarrarAhmad <akifsheikh84@gmail.com>
2025-11-22 22:57:00 +0000

    Riget Zoo Adventure

--- src/Pages/BookTicket/BookTicket.jsx
+++ src/Pages/BookTicket/BookTicket.jsx
@@ -4,10 +4,13 @@ import FooterSection from "../../component/FooterSection/footerSection"
 import birdImage from "../../assets/macaw-parrot-isolated-on-transparent-background-file-cut-out-ai-generated-png.png"
 import infoImag1 from "../../assets/Sibirischer_tiger_de_edit02.jpg"
 import infoImag2 from"../../assets/gettyimages-1219403220-1024x1024.jpg"
-
+import DayTicket from "../../component/DayTicket/DayTicket"
 
 
 export default function BookTicket() {
+
+    const [showPopup, setShowPopup] = useState(false)
+
     return (
         <>
             <HeaderSection />
@@ -29,7 +32,9 @@ export default function BookTicket() {
                 <div className="ticket-info-1">
                     <p className="box1-h1"><strong>Day Ticket - Truck ride is included</strong></p>
                     <p className="box1-h2">Adult 25.99, child 14.99</p>
-                    <a href="/">Book Now</a> 
+                    {/* <a href="/DayTicket">Book Now</a>  */}
+                    <a onClick ={()=> setShowPopup(true)}> Book Now</a>
+                                    {showPopup &&<DayTicket/>}
                 </div>
             </div>
             <div className="ticket-info-box-2">
---
+++ src/component/DayTicket/DayTicket.jsx
@@ -0,0 +1,7 @@
+import "./DayTicket.css"
+
+export default function DayTicket(){
+    return(
+        <p>Hello</p>
+    )
+}
